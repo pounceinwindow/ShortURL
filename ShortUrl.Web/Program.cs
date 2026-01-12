cf9db8c9715bc0ed8477866985170c955f9050de
pounceinwindow <adk03042006@gmail.com>
2026-01-13 00:38:56 +0300

    ref archi

---
+++ ShortUrl.Web/Program.cs
@@ -0,0 +1,17 @@
+using ShortUrl.Web.Extensions;
+using URLShortener.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAppServices(builder.Configuration, builder.Environment);
+
+var app = builder.Build();
+
+app.UseStaticFrontend();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapAppEndpoints();
+
+app.Run();
---
+++ ShortUrl.Web/wwwroot/js/createLink.js
@@ -0,0 +1,63 @@
+const token = localStorage.getItem("token");
+if (!token) location.href = "/login.html";
+
+document.querySelector("[data-logout]")?.addEventListener("click", () => {
+    localStorage.removeItem("token");
+});
+
+const btn = document.querySelector(".main__form__submit");
+const longInput = document.querySelector('input[name="long_link"]');
+const slugInput = document.querySelector('input[name="slug"]');
+const expiresInput = document.querySelector('input[name="expires"]');
+
+const linkEl = document.querySelector(".main__qr__link");
+const qrBox = document.querySelector(".main__qr__image");
+
+btn.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const originalUrl = longInput.value.trim();
+    const shortCode = slugInput.value.trim();
+    const expiresAt = (expiresInput?.value || "").trim();
+
+    if (!originalUrl) {
+        alert("Введите ссылку");
+        return;
+    }
+
+    const r = await fetch("/links", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify({ originalUrl, shortCode, expiresAt })
+    });
+
+    if (r.status === 401) {
+        localStorage.removeItem("token");
+        location.href = "/login.html";
+        return;
+    }
+    if (r.status === 409) {
+        alert("Такой slug уже занят");
+        return;
+    }
+    if (!r.ok) {
+        alert("Ошибка создания ссылки");
+        return;
+    }
+
+    const data = await r.json();
+
+    linkEl.textContent = data.shortUrl;
+    linkEl.href = data.shortUrl;
+    linkEl.target = "_blank"
+
+    qrBox.innerHTML = `<img alt="QR" src="/${data.shortCode}/qr?size=200">`;
+});
+
+document.querySelector(".main__qr__button").addEventListener("click", async () => {
+    const text = linkEl?.href;
+    if (text) await navigator.clipboard.writeText(text);
+});
